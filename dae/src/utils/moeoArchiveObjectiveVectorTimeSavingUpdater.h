cdcf6696f54654a0d616107e6b2f4d5b5747670e
quemy <alexandre.quemy@gmail.com>
2014-02-06 12:46:23 +0100

    Fix a bug introduced by the last commit.

--- dae/src/utils/moeoArchiveObjectiveVectorTimeSavingUpdater.h
+++ dae/src/utils/moeoArchiveObjectiveVectorTimeSavingUpdater.h
@@ -47,7 +47,7 @@ class moeoArchiveObjectiveVectorTimeSavingUpdater : public eoUpdater
       time_t now = time(0);
       char buff[MAX_BUFFER_SIZE];
 
-      if (now >= last_time + interval or first)
+      if (now >= last_time + interval)
       {
 	    last_time = now;
 	
